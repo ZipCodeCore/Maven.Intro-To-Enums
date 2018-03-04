bf7d32ee89965bd38b9220a93fb76c7c36f8a43f
Brian He <brianhe@zipcoders-MacBook-Pro-56.local>
2018-03-04 13:38:55 -0500

    progress?

--- src/main/java/com/zipcodewilmington/preassessment2/Person.java
+++ src/main/java/com/zipcodewilmington/preassessment2/Person.java
@@ -1,19 +1,20 @@
 package com.zipcodewilmington.preassessment2;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
-public class Person <T> {
+public class Person <T, E> {
 
+    Map<T, E> personInfo = new HashMap<>();
     T name;
     T age;
     T birthDate;
 
-    public Person (T name, T age, T birthDate) {
-        this.name = name;
-        this.age = age;
-        this.birthDate = birthDate;
+    public Person (E name, E age, E birthDate) {
+        personInfo.put(this.name, name);
+        personInfo.put(this.age, age);
+        personInfo.put(this.birthDate, birthDate);
+
     }
-
-
-
 }
