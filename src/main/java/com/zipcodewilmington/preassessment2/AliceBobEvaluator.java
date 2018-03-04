17252714314d817e2af7ea138736d90f3a74f7d9
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-04 12:23:51 -0500

    Saving

--- src/main/java/com/zipcodewilmington/preassessment2/AliceBobEvaluator.java
+++ src/main/java/com/zipcodewilmington/preassessment2/AliceBobEvaluator.java
@@ -1,4 +1,19 @@
 package com.zipcodewilmington.preassessment2;
 
 public class AliceBobEvaluator {
+
+    private String input;
+
+    public AliceBobEvaluator(String input){
+        this.input = input;
+    }
+
+    public boolean isAlice(){
+        return this.input.equalsIgnoreCase("Alice");
+    }
+
+    public boolean isBob(){
+        return this.input.equalsIgnoreCase("Bob");
+    }
+
 }
