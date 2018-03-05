a6594b2baf25e82fc66ae7d71b06cea562414994
Keith Brinker <keithbrinker@zipcoders-MBP-2.fios-router.home>
2018-03-04 21:14:16 -0500

    finished AliceBobEvaluator

--- src/main/java/com/zipcodewilmington/preassessment2/AliceBobEvaluator.java
+++ src/main/java/com/zipcodewilmington/preassessment2/AliceBobEvaluator.java
@@ -1,4 +1,25 @@
 package com.zipcodewilmington.preassessment2;
 
 public class AliceBobEvaluator {
+    String name;
+
+//    AliceBobEvaluator (){};
+
+    AliceBobEvaluator(String name){
+        this.name = name;
+    }
+    public boolean isAlice(){
+        if (name == "Alice"){
+            return true;
+        }
+        return false;
+    }
+
+    public boolean isBob(){
+        if (name == "Bob"){
+            return true;
+        }
+        return false;
+    }
+
 }
