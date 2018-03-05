1c42075b1e12e8ccc11daf921a9ece026c3951f6
Eric Barnaba <ericbarnaba@zipcoders-MacBook-Pro-25.local>
2018-03-04 19:04:40 -0500

    I hope this erases my shame

--- src/main/java/com/zipcodewilmington/preassessment2/Person.java
+++ src/main/java/com/zipcodewilmington/preassessment2/Person.java
@@ -3,14 +3,28 @@ package com.zipcodewilmington.preassessment2;
 import java.util.Date;
 
 public class Person {
-    public Names name;
-    public Ages age;
-    public BirthDates birthDate;
+
+    public Data<String> name;
+    public Data<Integer> age;
+    public Data<Date> birthDate;
 
     public Person(String name, int age, Date birthDate){
-        this.name = Names.Leon;
-        this.age = Ages.TWENTYFOUR;
-        this.birthDate = BirthDates.BIRTHDATE;
+        this.name = new Data(name);
+        this.age = new Data (age);
+        this.birthDate = new Data (birthDate);
+
+    }
+
+    public class Data <T>{
+         T value;
+        public Data(T input){
+            this.value =  input;
+        }
+
+        public T getValue(){
+            return this.value;
+        }
+
     }
 
 }
